d695b77801276bc939146ed912db1529d80f558a
MonMon201 <danyavasilinenko213@gmail.com>
2020-06-18 15:41:53 +0300

    feat: add send message option

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require('dotenv').config()
 const client = new Discord.Client();
  
 client.on('message', message => {
-    console.log(message.content);
+    if(message.content === 'g'){
+        const channelID = message.channel.id;
+        const channel = client.channels.cache.get(channelID);
+        channel.send("Option 1");
+        channel.send("Option 2");
+        channel.send("Option 3");
+    }
 });
 
 client.login(process.env.TOKEN);
